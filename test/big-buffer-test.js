ecb1802e161aea3c8647937f22a0a75b5ba036d7
Guglielmo Ferri <44gatti@gmail.com>
2012-10-29 00:51:11 +0100

    Added random index for big-buffer-test On branch master Changes to be committed: modified:   test/big-buffer-test.js

--- test/big-buffer-test.js
+++ test/big-buffer-test.js
@@ -25,14 +25,15 @@ log( '- patterns written:', indexes.length );
 log( '- parse data from beginning..' );
 var sptime = Date.now(),
     results = qap.parse( data ),
-    presults = null;
+    presults = null,
+    offset = Math.floor( results.length * Math.random() );
 log( '\n- input data was parsed in', ( ( Date.now() - sptime ) / 1000 ).toFixed( 2 ), 'secs' );
 log( '- total results:', results.length );
 assert.equal( results.length, indexes.length, 'parsed results don\'t match with existing patterns' );
-log( '- comparing parsed results and pre-recorded indexes' );
 assert.deepEqual( results, indexes, 'indexes and parsed indexes don\'t match' );
-log( '\n- re-parse data from 100th result index (' + indexes[ 100 ] + ') to obtain partial results..' );
-presults = qap.parse( data, indexes[ 100 ] + 1 );
-log( '- check partial results length (' + ( presults.length + 101 ) + ' === ' + results.length + ')' );
-assert.equal( presults.length + 101, results.length, 'results don\'t match' );
+log( '- sucessfully compared parsed results and pre-recorded indexes..' );
+log( '\n- re-parse data from ' + offset + ' result index (' + indexes[ offset ] + ') to obtain partial results..' );
+presults = qap.parse( data, indexes[ offset ] + 1 );
+log( '- check partial results length (' + ( presults.length ) + ' === ' + ( results.length - offset - 1 ) + ')' );
+assert.equal( presults.length + offset + 1, results.length, 'results don\'t match' );
 log( '\n- all tests passed in',( ( Date.now() - stime ) / 1000 ).toFixed( 2 ), 'secs\n');
