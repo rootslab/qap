2760986fcd36e52eff93d1f9c2415e53595578f9
Guglielmo Ferri <44gatti@gmail.com>
2012-10-29 23:05:04 +0100

    string arg handling in parse().. On branch master Changes to be committed: modified:   lib/qap.js modified:   package.json modified:   test/big-buffer-test.js

--- test/big-buffer-test.js
+++ test/big-buffer-test.js
@@ -1,4 +1,3 @@
-
 var log = console.log,
     assert = require( 'assert' ),
     QuickAsciiParser = require( '../' ).Qap,
@@ -15,25 +14,35 @@ var log = console.log,
     stime = Date.now();
 
 log( '- creating test buffer..(' + mb +' MB)' );
+
 for ( ; i < dlen - 1; i += occ ) {
     indexes.push( i );
     bpattern.copy( data, i );
 }
+
 log( '- buffer data copied in',( Date.now() - stime ) / 1000, 'secs' );
 log( '- pattern length:', bpattern.length, 'bytes' );
 log( '- patterns written:', indexes.length );
 log( '- parse data from beginning..' );
+
 var sptime = Date.now(),
     results = qap.parse( data ),
     presults = null,
     offset = Math.floor( results.length * Math.random() );
+
 log( '\n- input data was parsed in', ( ( Date.now() - sptime ) / 1000 ).toFixed( 2 ), 'secs' );
 log( '- total results:', results.length );
+
 assert.equal( results.length, indexes.length, 'parsed results don\'t match with existing patterns' );
 assert.deepEqual( results, indexes, 'indexes and parsed indexes don\'t match' );
+
 log( '- successfully compared parsed results and pre-recorded indexes..' );
 log( '\n- re-parse data from ' + offset + ' result index (' + indexes[ offset ] + ') to obtain partial results..' );
+
 presults = qap.parse( data, indexes[ offset ] + 1 );
+
 log( '- check partial results length (' + ( presults.length ) + ' === ' + ( results.length - offset - 1 ) + ')' );
+
 assert.equal( presults.length + offset + 1, results.length, 'results don\'t match' );
+
 log( '\n- all tests passed in',( ( Date.now() - stime ) / 1000 ).toFixed( 2 ), 'secs\n');
